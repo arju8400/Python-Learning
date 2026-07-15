5d24ec74a57ab6700322f748185f3da0a17c1e7a
arju8400 <arju840044@gmail.com>
2026-07-16 00:25:09 +0530

    Day 3- Learned how to find the area of different shapes by taking input from the users

---
+++ Level_1/areas.py
@@ -0,0 +1,13 @@
+radius = float(input("Enter the radius: " ))
+length = int(input("Enter the length: " ))
+breadth = int(input("Enter the breadth: " ))
+
+area1 = 3.14 * radius *radius
+area2 = 0.5 * length * breadth
+area3 = length * breadth
+area4 = length * length
+
+print("Area of Circle:",area1)
+print("Area of Triangle:",area2)
+print("Area of Rectangle:",area3)
+print("Area of Square:",area4)
