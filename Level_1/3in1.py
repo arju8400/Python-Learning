ca5fdc94b9686c20c5c3f2a5c9914ac8eecb7591
arju8400 <arju840044@gmail.com>
2026-07-16 23:39:36 +0530

    Learned to activate venv and concepts of literals in python

---
+++ Level_1/3in1.py
@@ -0,0 +1,10 @@
+print("Hello, You are welcome to learn python with me")
+
+message = "Welcome to Python programming"
+print("Message")
+
+
+Num1= 20
+Num2= 30
+
+print("Addition of this two Numbers:",Num1+Num2)
