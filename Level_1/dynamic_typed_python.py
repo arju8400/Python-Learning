fa19cfcea49f594cdd05497edbcfe976a693832b
arju8400 <arju840044@gmail.com>
2026-07-17 00:00:14 +0530

    Learned to use f string in python, and why python is called as the dynamically typped langauge

---
+++ Level_1/dynamic_typed_python.py
@@ -0,0 +1,8 @@
+x = "Arzoo Pandey"
+y = 20
+z = 13.10
+
+print(f"Name of the learner is {x}",f"Age of the learner is {y}",f" date and month of the birth of that learner is {z}")
+print(type(x))
+print(type(y))
+print(type(z))
