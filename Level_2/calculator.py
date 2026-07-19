f769aa18eacce1b37c64c36d43cc2cff09a1fd23
arju8400 <arju840044@gmail.com>
2026-07-19 23:14:05 +0530

    Basic Calculator By Python

---
+++ Level_2/calculator.py
@@ -0,0 +1,14 @@
+print("-------------------Calculator-------------------")
+a= int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = a + b
+      
+d= a - b
+e= a * b
+f= a / b
+print("Addition of two numbers is: ", c)
+print("Subtraction of two numbers is: ", d)
+print("Multiplication of two numbers is: ", e)
+print("Division of two numbers is: ", f)
+    
+    
